4362446d0152b07c9201f9828d26e902652f7fe2
Utilisateur <xolott@hotmail.fr>
2022-11-03 13:45:42 +0100

    second commit

---
+++ app/server.js
@@ -0,0 +1,27 @@
+// const http = require("http");
+
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.end("Hello World\n");
+//   })
+//   .listen(process.env.PORT || 8080, "127.0.0.1");
+
+// console.log("Server currently listening...");
+
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .send('Hello server is running')
+    .end();
+});
+ 
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
