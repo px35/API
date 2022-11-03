4ff544e778e77897ee97f62ca965ee7eee4cd09b
Utilisateur <xolott@hotmail.fr>
2022-11-03 14:15:52 +0100

    second commit

--- app/server.js
+++ app/server.js
@@ -9,19 +9,19 @@
 
 // console.log("Server currently listening...");
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
  
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running (app)')
-    .end();
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .send('Hello server is running (app)')
+//     .end();
+// });
  
 // Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}`);
+//   console.log('Press Ctrl+C to quit.');
+// });
